67779bd8c14b4b9d98a50148474243ffcbd968b1
xueyang0312 <jeak831130015@gmail.com>
2023-02-23 00:39:58 +0800

    Delete the middle node in queue

--- queue.c
+++ queue.c
@@ -135,6 +135,22 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *slow = head->next;
+    struct list_head *fast = head->next;
+
+    while (fast != head->prev && fast != head) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    element_t *node = list_entry(slow, element_t, list);
+    list_del(slow);
+    free(node->value);
+    free(node);
+
     return true;
 }
 
