7e44b407074bb46eb29640093abcf7476da83f2e
xueyang0312 <jeak831130015@gmail.com>
2023-02-22 23:38:20 +0800

    Count number of elements in queue

--- queue.c
+++ queue.c
@@ -104,7 +104,15 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || list_empty(head))
+        return -1;
+
+    int len = 0;
+    struct list_head *tmp;
+    list_for_each (tmp, head)
+        len++;
+
+    return len;
 }
 
 /* Delete the middle node in queue */
