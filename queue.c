0a1ecf0dd5f506503d33adb0f63a016f0122103c
xueyang0312 <jeak831130015@gmail.com>
2023-02-24 19:58:44 +0800

    Sort elements of queue in ascending order

--- queue.c
+++ queue.c
@@ -242,7 +242,34 @@ void q_reverseK(struct list_head *head, int k)
 }
 
 /* Sort elements of queue in ascending order */
-void q_sort(struct list_head *head) {}
+void q_sort(struct list_head *head)
+{
+    struct list_head list_less, list_greater;
+    element_t *pivot, *item, *safe;
+
+    if (list_empty(head) || list_is_singular(head))
+        return;
+
+    INIT_LIST_HEAD(&list_less);
+    INIT_LIST_HEAD(&list_greater);
+
+    pivot = list_first_entry(head, element_t, list);
+    list_del(&pivot->list);
+
+    list_for_each_entry_safe (item, safe, head, list) {
+        if ((item->value - pivot->value) < 0)
+            list_move_tail(&item->list, &list_less);
+        else
+            list_move_tail(&item->list, &list_greater);
+    }
+
+    q_sort(&list_less);
+    q_sort(&list_greater);
+
+    list_add(&pivot->list, head);
+    list_splice(&list_less, head);
+    list_splice_tail(&list_greater, head);
+}
 
 /* Remove every node which has a node with a strictly greater value anywhere to
  * the right side of it */
